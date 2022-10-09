e5871eff3cf7aa93612726c8550f2ff29106d8c5
junsantilla <junsantilla@live.com>
2022-10-09 22:52:56 +0800

    Create user search component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import UserSearch from "../components/users/UserSearch";
 import UserResults from "../components/users/UserResults";
 
 function Home() {
 	return (
 		<div>
-			{/* Search Component */}
+			<UserSearch />
 			<UserResults />
 		</div>
 	);
